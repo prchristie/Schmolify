6a290f364ff57557bfd79f9f6a7d77d329b17246
Patrick Christie <patrickc@canva.com>
2023-05-04 16:47:08 +0930

    big cool stuff

---
+++ app/src/pages/[id].ts
@@ -0,0 +1,11 @@
+import { useRouter } from "next/router";
+
+
+export default async function dwa() {
+    const router = useRouter();
+    const id = router.query.id;
+
+    const res = await fetch(`http://localhost:8080/api/v1/schmol/${id}`);
+    console.log(res);
+    return <div>test</div>;
+}
--- src/main/resources/SQL/intro-link-table.sql
+++ src/main/resources/SQL/intro-link-table.sql
@@ -4,7 +4,8 @@ DROP DATABASE IF EXISTS schmolify;
 CREATE DATABASE schmolify;
 USE schmolify;
 
-CREATE TABLE schmol (
-    id VARCHAR(16) PRIMARY KEY,
+CREATE TABLE schmol
+(
+    id  VARCHAR(16) PRIMARY KEY,
     url VARCHAR(1024) NOT NULL
 );
